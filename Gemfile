abef6d782b445c46a73f908611b377aff56d4e13
stoplion <george@stoplion.com>
2015-12-07 23:38:32 -0800

    adding mechanize, quick hello world

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'roo'
-gem 'google_places'
+gem 'mechanize'
 gem 'sinatra'
--- app.rb
+++ app.rb
@@ -1,14 +1,21 @@
 require 'sinatra'
-require 'google_places'
 require 'roo'
-
-GP_API_KEY = 'AIzaSyDA8RxO4Lvf2Xpn-JUtivA8qpG4oqEF7VA'
+require 'mechanize'
+require 'open-uri'
 
 get '/' do
-  @client = GooglePlaces::Client.new(GP_API_KEY)
   @path_to_spreadsheet = 'spreadsheets/ProspectList_GooglePlaces.xlsx'
   @xlsx = Roo::Spreadsheet.open(@path_to_spreadsheet)
 
+  agent = Mechanize.new do |agent|
+    agent.user_agent_alias = 'Mac Safari'
+  end
+
+  agent.get('http://google.com/') do |page|
+    @search_result = page.form_with(:name => 'f') do |search|
+      search.q = 'Hello world'
+    end.submit
+  end
   erb :home
 end
 
@@ -16,22 +23,6 @@ end
 __END__
 
 @@home
-<% @xlsx.each_row_streaming(offset: 1, max_rows: 3) do |row| %>
-
-  <% restName = row[1].to_s == "NULL" ? "" : row[1] %>
-  <% restAddress = row[11].to_s == "NULL" ? "" : row[11] %>
-  <% restCity = row[12].to_s == "NULL" ? "" : row[12] %>
-  <% restState = row[13].to_s == "NULL" ? "" : row[13] %>
-  <% restLocation = "#{restAddress} #{restCity} #{restState}" %>
-
-  <% @foo = @client.spots_by_query('Pizza near Miami Florida') %>
-  <% puts @foo %>
-
-  <pre>
-    Name: <%= restName %><br/>
-    Address: <%= restLocation %><br/>
-    <span>------------------------------------------</span>
-  </pre>
-<% end %>
-
-
+  <% @search_result.links.each do |link| %>
+    <%= puts link.text %>
+  <% end %>
