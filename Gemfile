25978bbfa53f6474f74d129660ea16df10e6d733
stoplion <george@stoplion.com>
2015-12-07 23:54:38 -0800

    blah

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'sinatra'
 gem 'roo'
 gem 'mechanize'
-gem 'sinatra'
+gem 'byebug'
--- app.rb
+++ app.rb
@@ -2,20 +2,36 @@ require 'sinatra'
 require 'roo'
 require 'mechanize'
 require 'open-uri'
+require 'byebug'
+
 
 get '/' do
   @path_to_spreadsheet = 'spreadsheets/ProspectList_GooglePlaces.xlsx'
   @xlsx = Roo::Spreadsheet.open(@path_to_spreadsheet)
 
-  agent = Mechanize.new do |agent|
+  @agent = Mechanize.new do |agent|
     agent.user_agent_alias = 'Mac Safari'
   end
 
-  agent.get('http://google.com/') do |page|
-    @search_result = page.form_with(:name => 'f') do |search|
-      search.q = 'Hello world'
-    end.submit
+  @xlsx.each_row_streaming(offset: 1, max_rows: 3) do |row|
+    restName = row[1].to_s == "NULL" ? "" : row[1]
+    restAddress = row[11].to_s == "NULL" ? "" : row[11]
+    restCity = row[12].to_s == "NULL" ? "" : row[12]
+    restState = row[13].to_s == "NULL" ? "" : row[13]
+    restLocation = "#{restName} #{restAddress} #{restCity} #{restState}"
+
+
+    @agent.get('http://google.com/') do |page|
+      @search_result = page.form_with(:name => 'f') do |search|
+        search.q = restLocation
+      end.submit
+      byebug
+      restDescription = page.at('._N1d')
+    end
   end
+
+
+
   erb :home
 end
 
